c7ad6f3804e994b1dbf9ef68500aecbbddbd5097
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-05-07 15:58:22 +0530

    big bang
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+           function (registration) {
+             console.log("[SW] Registered with scope : ", registration.scope);
+           },
+           function (err) {
+             console.log("[SW] Registration failed! : ", err);
+           }
+         );
+       });
+    }
+  })
+  return <>
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('fetch', function (event) {
+	if (event.request.destination !== 'images') return;
+	if (!('caches' in window)) return;
+  event.respondWith(
+    caches.open('images').then(async function(cache) {
+      const response = await cache.match(event.request);
+			return response || fetch(event.request).then(function (response_1) {
+				cache.put(event.request, response_1.clone());
+				return response_1;
+			});
+    })
+  );
+});
